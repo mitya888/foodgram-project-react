21e6e4bb4f812372ca1402d21fa0c175604a3b4c
mitya888 <dbryndin@inbox.ru>
2022-02-01 23:57:07 +0300

    v0.3.1

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,59 +1,46 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
-from rest_framework import generics, status, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.db.models import Count
-from drf_yasg.utils import swagger_auto_schema
-
 
 from .models import Follow
-from .pagination import BasePageNumberPagination
-from . import serializers
-
+from .serializers import FollowSerializer, ShowFollowSerializer
+from .paginator import CustomPageNumberPaginator
 
 User = get_user_model()
 
 
-class FollowApiView(generics.GenericAPIView):
+class FollowApiView(APIView):
     permission_classes = [permissions.IsAuthenticated, ]
 
-    def get_queryset(self):
-        return User.objects.all()
+    def get(self, request, id):
+        data = {'user': request.user.id, 'author': id}
+        serializer = FollowSerializer(data=data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def post(self, request, pk: int):
-        author = self.get_object()
-        request.user.following.add(author)
-
-        # data = {'user': request.user.id, 'author': pk}
-        # serializer = FollowSerializer(data=data, context={'request': request})
-        # serializer.is_valid(raise_exception=True)
-        # serializer.save()
-        return Response({'&&&'}, status=status.HTTP_201_CREATED)
-
-    def delete(self, request, pk: int):
-        author = self.get_object()
-        request.user.following.remove(author)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, id):
+        user = request.user
+        author = get_object_or_404(User, id=id)
+        try:
+            subscription = Follow.objects.get(user=user, author=author)
+            subscription.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Follow.DoesNotExist:
+            return Response(
+                'Ошибка отписки',
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
 
-class ListFollowView(generics.ListAPIView):
-    serializer_class = serializers.ListFollowingSerializer
-    pagination_class = BasePageNumberPagination
+class ListFollowViewSet(generics.ListAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated, ]
+    serializer_class = ShowFollowSerializer
+    pagination_class = CustomPageNumberPaginator
 
     def get_queryset(self):
         user = self.request.user
-        return user.following.all().annotate(recipes_count=Count('recipes'))
-
-    @swagger_auto_schema(query_serializer=serializers.RecipeLimitSerializer)
-    def get(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-        # return super().get(request, *args, **kwargs)
+        return User.objects.filter(following__user=user)
