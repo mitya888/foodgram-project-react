6afc5fe67993e8c1425e0cbac589c37cf189b505
mitya888 <dbryndin@inbox.ru>
2022-02-01 23:52:07 +0300

    v0.2.1

---
+++ backend/recipes/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsAuthorOrAdmin(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return request.user.is_authenticated
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_authenticated and (
+                request.user.is_superuser
+                or obj.author == request.user
+                or request.method == 'POST'
+        ):
+            return True
+        return request.method in
