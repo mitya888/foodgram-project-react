0508b31a0248f25d1a5b54bb010bfad46a3ca52f
mitya888 <dbryndin@inbox.ru>
2022-02-01 23:42:52 +0300

    v0.2 - Edit:Settings-Rest_framework

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -92,12 +92,20 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 DJOSER = {
     'LOGIN_FIELD': 'email',
+    'SERIALIZERS': {
+            'user_create': 'users.serializers.UserRegistrationSerializer',
+            'user': 'users.serializers.CustomUserSerializer',
+            'current_user': 'users.serializers.CustomUserSerializer',
+        },
 }
 
 LANGUAGE_CODE = 'ru'
