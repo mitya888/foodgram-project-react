3265a59c97d452b0b7f9338d21f9696f43cb0d00
mitya888 <dbryndin@inbox.ru>
2022-02-02 21:53:39 +0300

    v0.4 edit views

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,8 +1,20 @@
 from django.urls import include, path
+from djoser.views import TokenCreateView, TokenDestroyView
 
-from .views import ListFollowView, FollowApiView
+from .views import FollowApiView, ListFollowViewSet
 
 urlpatterns = [
-    path('users/subscriptions/', ListFollowView.as_view(), name='following_list'),
-    path('users/<pk>/subscribe/', FollowApiView.as_view(), name='subscribe_list'),
+
+
+    path('auth/token/login/', TokenCreateView.as_view(), name='login'),
+    path('auth/token/logout/', TokenDestroyView.as_view(), name='logout'),
+
+    path('users/<int:id>/subscribe/',
+         FollowApiView.as_view(),
+         name='subscribe'
+         ),
+    path('users/subscriptions/', ListFollowViewSet.as_view(),
+         name='subscription'
+         ),
+    path('', include('djoser.urls')),
 ]
