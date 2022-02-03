779e8392cc2f4a7ee3ece0e67c0b348d0cbc71d6
mitya888 <dbryndin@inbox.ru>
2022-02-03 18:38:50 +0300

    v0.6 filters edit 5

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,6 +1,6 @@
 import django_filters as filters
 
-from .models import Ingredient
+from .models import Ingredient, Recipe
 
 
 class IngredientsFilter(filters.FilterSet):
@@ -12,3 +12,38 @@ class IngredientsFilter(filters.FilterSet):
     class Meta:
         model = Ingredient
         fields = ('name',)
+
+
+class RecipeFilter(filters.FilterSet):
+    tags = filters.AllValuesMultipleFilter(
+        field_name='tags__slug',
+        lookup_expr="iexact",
+        label='Tags',
+    )
+    is_favorited = filters.BooleanFilter(
+        method='get_favorite',
+        label='Favorited',
+    )
+    is_in_shopping_cart = filters.BooleanFilter(
+        method='get_shopping',
+        label='Is in shopping list',
+    )
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'is_favorited',
+            'author',
+            'tags',
+            'is_in_shopping_cart',
+        )
+
+    def get_favorite(self, queryset, name, item_value):
+        if self.request.user.is_authenticated and item_value:
+            queryset = queryset.filter(in_favorite__user__user=self.request.user)
+        return queryset
+
+    def get_shopping(self, queryset, name, item_value):
+        if self.request.user.is_authenticated and item_value:
+            queryset = queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
