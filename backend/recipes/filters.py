28ed7f1a30874fa0ee8d647ae0329aecc9876c35
mitya888 <dbryndin@inbox.ru>
2022-03-04 18:59:41 +0300

    review v2.2

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -40,7 +40,7 @@ class RecipeFilter(filters.FilterSet):
 
     def get_favorite(self, queryset, name, item_value):
         if self.request.user.is_authenticated and item_value:
-            queryset = queryset.filter(in_favorite__user__user=self.request.user)
+            queryset = queryset.filter(in_favorite__user=self.request.user)
         return queryset
 
     def get_shopping(self, queryset, name, item_value):
